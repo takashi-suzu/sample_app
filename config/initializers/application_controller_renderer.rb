2d295b0785b98659d5433b0bc27361d157448e9f
takashi-suzu <t.suzuki33333@gmail.com>
2020-05-05 20:28:33 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6450d316e2262df0877a3f384332ee1e9dab7adc0b66653242412c859bb48faa6f5e5f08db9ef661813c70071bf1bc34552a826b28dab98fa4d3e3c5dccfde18'
